b56fe55ed7dbfd3287bc76e338a571decf40fab1
Iago Valverde <iagopachiani96@gmail.com>
2023-04-24 12:36:54 -0300

    Create integrador_02.py

---
+++ Programacion_I/integradores/integrador_02.py
@@ -0,0 +1,18 @@
+'''
+Desafio Integrador 02
+Nombre: Iago Valverde Pachiani
+Div - 1D
+'''
+
+from data_stark import lista_personajes
+
+def stark_normalizar_datos(lista_personajes):
+    if len(lista_personajes) == 0:
+        print("Error: Lista de héroes vacía")
+    
+    for heroe in lista_personajes:
+        
+
+
+
+#stark_normalizar_datos(lista_personajes)
